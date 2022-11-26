8b9e0ad7af70df7e29b73eac3052f8e9e6fd5356
ssnaruka <surendranaruka33@gmail.com>
2022-11-26 23:20:17 +0530

    -setup connection between bottom nav with navigation component

--- app/src/main/java/com/surendrasingh/bottomnavdemo/ui/MainActivity.kt
+++ app/src/main/java/com/surendrasingh/bottomnavdemo/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package com.surendrasingh.bottomnavdemo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.surendrasingh.bottomnavdemo.R
 import com.surendrasingh.bottomnavdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupNavigationView()
+    }
+
+    private fun setupNavigationView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
